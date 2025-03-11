9fac0ae4041deed20d5572db8ecad1c4c33f325d
1905867 <1905867@01-18127.pusd.dom>
2025-03-11 14:08:12 -0700

    worked on map

--- P2_first_last/src/Map.java
+++ P2_first_last/src/Map.java
@@ -1,14 +1,54 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Map {
+	private Tile[][][] map;
 	
 	public Map(String map) {
 		File file = new File(map);
-		
+		try {
+			Scanner scan = new Scanner(file);
+			// get dimensions
+			int rows = scan.nextInt();
+			int cols = scan.nextInt();
+			int levels = scan.nextInt();
+			
+			
+			
+			this.map = new Tile[rows][cols][levels];
+			
+			//fill map
+			String line = scan.nextLine();
+			for(int i = 1; i<=levels; i++) {
+				for(int j = 0; j<rows; j++) {
+					for(int k = 0; k<cols; k++) {
+						this.map[i][j][k] = new Tile(j,k,line.charAt(k));
+					}
+					line = scan.nextLine();
+				}
+			}
+			scan.close();
+		}
+		catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
-	
+	public String toString() {
+		String res = "";
+		for(int i = 1; i<=map.length;i++) {
+			for(int j = 0; j<map[0].length; j++) {
+				for(int k = 0; k<map[0][0].length; k++) {
+					res += map[i][j][k] + " ";
+				}
+				res += "/n";
+			}
+		}
+		return res;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("p2");
+		Map room1 = new Map("src/test1");
+		System.out.println(room1.toString());
 	}
 }
---
+++ P2_first_last/src/Tile.java
@@ -0,0 +1,36 @@
+
+public class Tile {
+	private int row;
+	private int col;
+	private char location;
+	
+	public Tile (int row, int col, char location) {
+		this.row = row;
+		this.col = col;
+		this.location = location;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getCol() {
+		return col;
+	}
+
+	public void setCol(int col) {
+		this.col = col;
+	}
+
+	public char getLocation() {
+		return location;
+	}
+
+	public void setLocation(char location) {
+		this.location = location;
+	}
+}
