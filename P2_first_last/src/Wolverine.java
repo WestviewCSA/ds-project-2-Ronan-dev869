7f708782f0a95800d0f162e37f7b7675cdab0496
1905867 <1905867@01-18127.pusd.dom>
2025-03-13 14:07:08 -0700

    figured out movement roughly, adjust parameters (likely rewrite a good bit) finish writing west for stack (PUT IN TO NEW OBJ)

--- P2_first_last/src/Wolverine.java
+++ P2_first_last/src/Wolverine.java
@@ -30,36 +30,112 @@ public class Wolverine {
 		this.z = z;
 	}
 	
-//	public Stack<Tile> getPath(Tile[][][] board) {
-//		Stack<Tile> moves = new Stack<Tile>();
-//		moves = north(x,y,z,board);
-//		return moves;
-//	}
-//	
-//	public Stack<Tile> north(int x, int y, int z, Tile[][][] board){
-//		if(board[z][x][y].getLocation() == '$') {
-//			return path;
-//		}
-//		if(board[z][x][y].getLocation() == '@' || x<0) {
-//			deNorth(x, board);
-//			return path;
-//		}
-//		else {
-//			path.push(new Tile(x-1,y,'+'));
-//			north(x-1,y,z,board);
-//			return path;
-//		}
-//	}
-//	
-//	public void deNorth(int x, Tile[][][] board){
-//		if(board[z][x][y+1].getLocation() == '.' && y+1<board[0][0].length) {
-//			return;
-//		}
-//		if(board[z][x][y-1].getLocation() == '.' && y>0) {
-//			return;
-//		}
-//		else {
-//			path.pop();
-//		}
-//	}
+	public Stack<Tile> getPath(Tile[][][] board) {
+		
+		if(board[z][x-1][y].getLocation() == '@') {
+			 east(x, y,board);
+		}
+		
+		System.out.println(path);
+		return path;
+	}
+	
+	public Stack<Tile> north(int x, Tile[][][] board){
+		System.out.println(x);
+		if(board[z][x][y].getLocation() == '$') {
+			return path;
+		}
+		if(board[z][x-1][y].getLocation() == '@' || x<0) {
+			deNorth(x, board);
+			return path;
+		}
+		else {
+			path.push(new Tile(x-1,y,'+'));
+			north(x-1,board);
+			
+			return path;
+		}
+	}
+	
+	public void deNorth(int x, Tile[][][] board){
+		if(board[z][x][y+1].getLocation() == '.' && y+1<board[0][0].length) {
+			east(x, y+1,board);
+			return;
+		}
+		if(board[z][x][y-1].getLocation() == '.' && y>0) {
+			return;
+		}
+		else {
+			path.pop();
+			deNorth(x+1,board);
+		}
+	}
+	
+	public Stack<Tile> east(int x, int y, Tile[][][] board){
+		if(board[z][x][y+1].getLocation() == '$') {
+			return path;
+		}
+		if(board[z][x][y+1].getLocation() == '@' || y>board[0][0].length) {
+			deEast(x, y, board);
+			return path;
+		}
+		else {
+			System.out.println("yes");
+			path.push(new Tile(x,y+1,'+'));
+			east(x,y+1, board);
+			return path;
+		}
+	}
+	public void deEast(int x, int y, Tile[][][] board){
+		if(board[z][x][y].getLocation() == '+') {
+			deNorth(x,board);
+			return;
+		}
+		if(board[z][x-1][y].getLocation() == '.' && x>0) {
+			north(x-1,board);
+			return;
+		}
+		if(board[z][x+1][y].getLocation() == '.' && x<board[0].length) {
+			south(x+1, y, board);
+			return;
+		}
+		else {
+			path.pop();
+			
+			return;
+		}
+	}
+	public Stack<Tile> south(int x, int y, Tile[][][] board){
+		if(board[z][x][y].getLocation() == '$') {
+			return path;
+		}
+		if(board[z][x][y].getLocation() == '@' || x>board[0].length) {
+			deSouth(x, y, board);
+			return path;
+		}
+		else {
+			
+			path.push(new Tile(x+1,y,'+'));
+			south(x+1, y, board);
+			return path;
+		}
+	}
+	public void deSouth(int x, int y, Tile[][][] board){
+		if(board[z][x][y].getLocation() == '+') {
+			deNorth(x,board);
+			return;
+		}
+		if(board[z][x][y+1].getLocation() == '.' && y+1<board[0][0].length) {
+			east(x, y+1,board);
+			return;
+		}
+		if(board[z][x][y-1].getLocation() == '.' && y>0) {
+			return;
+		}
+		else {
+			path.pop();
+			deSouth(x-1, y, board);
+			return;
+		}
+	}
 }
--- P2_first_last/src/runner.java
+++ P2_first_last/src/runner.java
@@ -5,12 +5,15 @@ public class runner{
 		// TODO Auto-generated method stub
 		Map mazes = new Map("src/test1");
 		Wolverine wol = new Wolverine(mazes.getX(), mazes.getY(), mazes.getZ());
-		//System.out.println(wol.getX() + " " + wol.getY() + " " + wol.getZ());
-//		while(moves.size() >0) {
-//			Tile move = moves.pop();
-//			mazes.setTile(move.getRow(), move.getCol(), 0, move.getLocation());
-//		}
-		mazes.getMap().toString();
+		System.out.println(wol.getX() + " " + wol.getY() + " " + wol.getZ());
+		Stack<Tile> moves = new Stack<Tile>();
+		moves = wol.getPath(mazes.getMap());
+		System.out.println(moves);
+		while(moves.size() >0) {
+			Tile move = moves.pop();
+		mazes.setTile(move.getRow(), move.getCol(), 0, move.getLocation());
+		}
+		System.out.println(mazes.toString());
 	}
 
 }
